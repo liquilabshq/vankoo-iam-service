9bcfd88348cd99d34e1f074885b72eb5b240d132
Salim Ramirez <salimramirezm@gmail.com>
2026-02-15 14:44:13 -0500

    chore: commit inicial

---
+++ src/main/java/com/liquilabs/vankoo/iam/VankooIamServiceApplication.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VankooIamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VankooIamServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liquilabs/vankoo/iam/VankooIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquilabs.vankoo.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VankooIamServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
