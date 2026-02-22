3710416d1b892e5f35285ddae41b0fdea8e7d7aa
Salim Ramirez <salimramirezm@gmail.com>
2026-02-22 11:53:20 -0500

    feat: estructura inicial con Scalar y API de HelloWorld

---
+++ src/main/java/com/liquilabs/vankoo/iam/interfaces/rest/controllers/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.liquilabs.vankoo.iam.interfaces.rest.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloWorldController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
