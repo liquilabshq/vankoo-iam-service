0ef2806ea39bd72b3272f38654bf37d97f0e0893
Salim Ramirez <salimramirezm@gmail.com>
2026-03-09 19:19:01 -0500

    fix: cambiar UserDetailsServiceImpl para usar UserId en vez de Email

--- src/main/java/com/liquilabs/vankoo/iam/infrastructure/authorization/sfs/services/UserDetailsServiceImpl.java
+++ src/main/java/com/liquilabs/vankoo/iam/infrastructure/authorization/sfs/services/UserDetailsServiceImpl.java
@@ -1,12 +1,14 @@
 package com.liquilabs.vankoo.iam.infrastructure.authorization.sfs.services;
 
-import com.liquilabs.vankoo.iam.domain.model.valueobjects.Email;
+import com.liquilabs.vankoo.iam.domain.model.valueobjects.UserId;
 import com.liquilabs.vankoo.iam.infrastructure.authorization.sfs.model.UserDetailsImpl;
 import com.liquilabs.vankoo.iam.infrastructure.persistence.jpa.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service(value = "defaultUserDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,8 +21,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userRepository.findByEmail(new Email(username))
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        var userId = new UserId(UUID.fromString(username));
+        var user = userRepository.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + username));
         return UserDetailsImpl.build(user);
     }
 }
